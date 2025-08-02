585f8740eafa83f7fd19850dfafbc63422be3be6
Manvi Madan <manvimadan@outlook.com>
2025-08-02 13:18:04 +1200

    reviewing C++ basics

---
+++ LetusC/BackToBasics.cpp
@@ -0,0 +1,13 @@
+#include <iostream> // For input and output
+#include <string> //For using strings
+using namespace std; // so we can write count instead of std::cout
+
+int x = 5; //integer
+double pi = 3.14; // Floating point number
+char grade = 'A'; // Sigle character
+string greeting = "Hello"; // Text string
+bool isHappy = true; //Boolean True or False
+
+int main() {
+cout << greeting << endl; 
+}
